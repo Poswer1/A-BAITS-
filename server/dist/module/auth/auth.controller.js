6ed24238187a4b101f18307922eb154d061eb633
Power1 <knozenko2@gmail.com>
2026-03-26 23:58:42 +0200

    master

--- server/dist/module/auth/auth.controller.js
+++ server/dist/module/auth/auth.controller.js
@@ -37,6 +37,7 @@ let AuthController = class AuthController {
             path: '/',
             maxAge: 1000 * 60 * 60 * 24 * 7
         });
+        console.log('cookies:', res.cookie);
         return { ok: true };
     }
 };
